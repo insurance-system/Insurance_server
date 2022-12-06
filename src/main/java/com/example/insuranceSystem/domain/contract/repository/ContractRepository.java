7e53455a1b352ddaed12638806d95e5eca080748
sojungpp <sophie2458@naver.com>
2022-12-06 17:19:09 +0900

    #38 메소드 이름 변경

--- src/main/java/com/example/insuranceSystem/domain/contract/repository/ContractRepository.java
+++ src/main/java/com/example/insuranceSystem/domain/contract/repository/ContractRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
-    List<Contract> findByCustomer_id(Long id);
+    List<Contract> findByCustomerId(Long id);
 }
