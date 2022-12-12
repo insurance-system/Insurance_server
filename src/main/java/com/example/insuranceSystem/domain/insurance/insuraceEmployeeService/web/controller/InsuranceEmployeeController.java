7bb23dc2cb0acb7e922a37ddcad56edbd45db5c1
kyubeom <be_man@naver.com>
2022-12-12 17:30:28 +0900

    #84 기능별 부서 역할 주석 추가

--- src/main/java/com/example/insuranceSystem/domain/insurance/insuraceEmployeeService/web/controller/InsuranceEmployeeController.java
+++ src/main/java/com/example/insuranceSystem/domain/insurance/insuraceEmployeeService/web/controller/InsuranceEmployeeController.java
@@ -36,12 +36,14 @@ public class InsuranceEmployeeController {
         return insuranceService.getInsurance(id);
     }
 
+    //상품개발팀
     @Operation(summary = "보험 생성하기(완)", description = "보험 생성하기")
     @PostMapping("/development")
     public Header<InsuranceResponse> developInsurance(@RequestBody @Valid InsuranceSaveRequest insuranceSaveRequest, HttpServletRequest request){
         return insuranceService.create(insuranceSaveRequest, request);
     }
 
+    //영업팀
     /*1-2*/
     @Operation(summary = "상담대기 신규고객 명단 조회(완)", description = "상담 대기 신규 고객 명단 조회")
     @GetMapping("/sales/new-customer")
@@ -49,6 +51,7 @@ public class InsuranceEmployeeController {
         return insuranceService.getContractCustomer(request);
     }
 
+   //영업팀
     //TODO 보험 관심자는 보험 상담 요청에 해당하는건가?
     @ApiOperation(value = "보험 관심자 배정", notes = "보험 관심자 배정")
     @PostMapping("/sales/interest")    // insurance-employee/sales
@@ -56,24 +59,28 @@ public class InsuranceEmployeeController {
         return insuranceService.assignInsuranceInterested(request, insuranceNum);
     }
 
+    //사후관리팀
     @ApiOperation(value = "계약 기간 만료 임박 리스트 출력(완)", notes = "계약 기간 만료 임박 리스트 출력")
     @GetMapping("/contract/soon-expiration") // 계약 기간 만료 임박
-    public Header<List<ContractCustomerResponse>> getNearExpireContractList(HttpServletRequest request){
+    public Header<List<ContractCustomerResponse>> ㅌ(HttpServletRequest request){
         return insuranceService.getNearExpireContractList(request);
     }
 
+    //사후관리팀
     @ApiOperation(value = "보험 납부 기간 만료 임박 조회(완)", notes = "보험 납부 기간 만료 임박")
     @GetMapping("/contract/payment-soon-expiration") //보험 납부 기간 만료 임박
     public Header<List<ContractCustomerResponse>> notifyContractStatus(HttpServletRequest request){
         return insuranceService.notifyContractStatus(request);
     }
 
+    //계약관리팀
     @ApiOperation(value = "보험 만기 고객 조회",notes = "보험 만기 고객 조회//건강정보 테이블에 고객 ID 들어가야함")
     @GetMapping("/followup/expiration")
     public Header<List<ContractCustomerResponse>> printExpirationContract(HttpServletRequest request){
         return insuranceService.printExpirationContract(request);
     }
 
+    //계약관리팀
     @ApiOperation(value = "미납 고객 조회", notes = "미납 고객 조회")
     @GetMapping("/contract/non-payment")
     public Header<List<ContractCustomerResponse>> printNonPaymentContract(HttpServletRequest request){
@@ -87,6 +94,7 @@ public class InsuranceEmployeeController {
         return insuranceService.getLectureList(request);
     }
 
+    // 영업교육팀
     @Operation(summary = "영업 교육 강의 업로드", description = "영업 교육 강의를 업로드한다. header필요")
     @PostMapping("/education")
     public Header<Void> uploadEducationLecture(@RequestBody @Valid LectureRequest lectureRequest,
@@ -102,6 +110,7 @@ public class InsuranceEmployeeController {
         return insuranceService.getUwList(request);
     }
 
+    //UW팀
     /*1-6*/
     @Operation(summary = "인수심사 수행", description = "계약 리스트로 정보를 확인하고 contractId와 & contractStatus:정상/인수심사 거절/인수심사 진행중(보류일 때) 중 하나를 선택해 인수심사를 수행한다.")
     @PatchMapping("/uw")
@@ -123,6 +132,7 @@ public class InsuranceEmployeeController {
         return insuranceService.getIncidentLogList(request);
     }
 
+    //손해접수팀
     /*2-2*/
     @Operation(summary = "사고 접수 담당자 배정", description = "사고 접수된 리스트 중 하나를 선택해 담당자를 배정한다. header 필요")
     @PatchMapping("/damage/{incident_log_id}")
@@ -137,6 +147,7 @@ public class InsuranceEmployeeController {
         return insuranceService.getInsuranceClaimList(request);
     }
 
+    //보상평가팀
     @Operation(summary = "보상금 심사", description = "보상금 리스트 중 심사할 내역을 선택해 심사 비용을 등록한다.")
     @PatchMapping("/reward")
     public Header<Void> evaluateReward(@RequestBody EvaluateRewardRequest evaluateRewardRequest, HttpServletRequest request){
