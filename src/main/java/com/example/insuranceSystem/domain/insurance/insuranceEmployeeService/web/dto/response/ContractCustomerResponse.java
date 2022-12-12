8fd7cee24aea4bd6bfcd6c845fde998f9327c996
kyubeom <be_man@naver.com>
2022-12-12 22:27:34 +0900

    #84 InsuranceStatus추가

--- src/main/java/com/example/insuranceSystem/domain/insurance/insuranceEmployeeService/web/dto/response/ContractCustomerResponse.java
+++ src/main/java/com/example/insuranceSystem/domain/insurance/insuranceEmployeeService/web/dto/response/ContractCustomerResponse.java
@@ -2,6 +2,7 @@ package com.example.insuranceSystem.domain.insurance.insuranceEmployeeService.we
 
 import com.example.insuranceSystem.domain.common.repository.entity.Address;
 import com.example.insuranceSystem.domain.customerService.repository.entity.HealthInformation;
+import com.example.insuranceSystem.domain.insurance.repository.entity.enumeration.InsuranceStatus;
 import com.example.insuranceSystem.domain.insurance.repository.entity.enumeration.KindOfInsurance;
 import com.example.insuranceSystem.global.enumerations.KindOfJob;
 import lombok.Builder;
@@ -12,6 +13,7 @@ import lombok.Getter;
 public class ContractCustomerResponse {
     private String customerName;
     private String phoneNum;
+    private InsuranceStatus insuranceStatus;
     private Address address;
     private KindOfJob kindOfJob;
     private HealthInformation healthInformation;
@@ -21,9 +23,10 @@ public class ContractCustomerResponse {
     private KindOfInsurance kindOfInsurance;
 
     @Builder
-    public ContractCustomerResponse(String customerName, String phoneNum, Address address, KindOfJob kindOfJob, HealthInformation healthInformation, String insuranceName, int fee, KindOfInsurance kindOfInsurance) {
+    public ContractCustomerResponse(String customerName, String phoneNum, InsuranceStatus insuranceStatus, Address address, KindOfJob kindOfJob, HealthInformation healthInformation, String insuranceName, int fee, KindOfInsurance kindOfInsurance) {
         this.customerName = customerName;
         this.phoneNum = phoneNum;
+        this.insuranceStatus = insuranceStatus;
         this.address = address;
         this.kindOfJob = kindOfJob;
         this.healthInformation = healthInformation;
