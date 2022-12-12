d18b785d1cb2efa9aa363852a8daf8ff5e31f208
kingchan223 <kokiyo97@naver.com>
2022-12-12 13:30:31 +0900

    #34 InsuranceCustomerService 익센변 처리 완료

--- src/main/java/com/example/insuranceSystem/domain/insurance/exception/InsuranceExceptionEnum.java
+++ src/main/java/com/example/insuranceSystem/domain/insurance/exception/InsuranceExceptionEnum.java
@@ -18,7 +18,11 @@ public enum InsuranceExceptionEnum {
     NOT_FOUND_INCIDENT_LOG("IL001", HttpStatus.NOT_FOUND, "사고 접수 내역을 찾을 수 없습니다."),
     NO_INCIDENT_LOG("IL002", HttpStatus.NO_CONTENT, "사고 접수 내역이 없습니다."),
 
-    NO_LECTURE_LIST("L001", HttpStatus.NO_CONTENT, "등록된 영업 교육 강의 리스트가 없습니다.");
+    NO_LECTURE_LIST("L001", HttpStatus.NO_CONTENT, "등록된 영업 교육 강의 리스트가 없습니다."),
+
+    NOT_FOUND_CONSULT("C001", HttpStatus.NO_CONTENT, "등록된 상담내역이 없습니다."),
+    NO_JOINED_INSURANCE("I001", HttpStatus.NO_CONTENT, "해당 고객은 가입한 보험이 없습니다.");
+
 
     public final String CODE;
     public final HttpStatus httpStatus;
--- src/main/java/com/example/insuranceSystem/domain/insurance/exception/execute/NothingJoinedInsuranceException.java
+++ src/main/java/com/example/insuranceSystem/domain/insurance/exception/execute/NothingJoinedInsuranceException.java
@@ -1,5 +1,13 @@
 package com.example.insuranceSystem.domain.insurance.exception.execute;
 
-public class NothingJoinedInsuranceException extends RuntimeException{
+import com.example.insuranceSystem.domain.insurance.exception.InsuranceException;
+import org.springframework.http.HttpStatus;
 
+import static com.example.insuranceSystem.domain.insurance.exception.InsuranceExceptionEnum.NO_JOINED_INSURANCE;
+
+public class NothingJoinedInsuranceException extends InsuranceException {
+
+    public NothingJoinedInsuranceException() {
+        super(NO_JOINED_INSURANCE.getCODE(), NO_JOINED_INSURANCE.getHttpStatus(), NO_JOINED_INSURANCE.getMESSAGE());
+    }
 }
